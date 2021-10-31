08fcad86f2d10a4e8f453e6423255b193e06503a
nyarla <nyarla@kalaclista.com>
2021-10-31 10:43:46 +0900

    fix: fix wrong usage on lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,8 @@ local installer = require("nvim-lsp-installer")
 
 installer.on_server_ready(
   function(server)
+    local opts = {}
+    server:setup(opts)
     exec("do User LspAttachBuffers")
   end
 )
